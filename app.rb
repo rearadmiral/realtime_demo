56d2bec7492ce84755c8c16f514e4f41a77829e0
Bill DePhillips <bill.dephillips@gmail.com>
2014-04-04 11:23:00 -0700

    rack it up

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
-set :public_folder, File.dirname(__FILE__)
 
-get '/' do
-  redirect to 'realtime.html'
+
+class App < Sinatra::Base
+
+  set :public_folder, File.dirname(__FILE__)
+
+  get '/' do
+    redirect to 'realtime.html'
+  end
+
+
 end
